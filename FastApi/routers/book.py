0e11824cc72545b296dc08aa99d210cd6ed72e15
DenisYaroslavtsev <deniska7777@list.ru>
2024-12-27 23:06:31 +0300

    upd

--- FastApi/routers/book.py
+++ FastApi/routers/book.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from fastapi_pagination import Page
+from fastapi_pagination import Page, add_pagination
 import os
 
 router = APIRouter(prefix='/books', tags=['books'])
 templates = Jinja2Templates(directory="FastApi/templates")
-
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 BOOK_DIR = os.path.join(BASE_DIR, '../books')
@@ -28,7 +27,7 @@ def book_song_of_ice_and_fire():
 def reed_book1(request: Request, page: int = 1):
     book_lines = book_lev_tolskoi()
     total_lines = len(book_lines)
-    per_page = 38
+    per_page = 30
     start = (page - 1) * per_page
     end = start + per_page
 
@@ -37,8 +36,7 @@ def reed_book1(request: Request, page: int = 1):
     pagination = Page(page=page, total=total_lines, size=per_page, items=lines_to_display)
 
     return templates.TemplateResponse("L.Tolstoi.html",
-                                      {"request": request, "lines": lines_to_display, "pagination": pagination,
-                                       "current_page": page})
+                                      {"request": request, "lines": lines_to_display, "pagination": pagination})
 
 
 @router.get('/game_of_the_thrones')
@@ -54,10 +52,12 @@ def reed_book2(request: Request, page: int = 1):
     pagination = Page(page=page, total=total_lines, size=per_page, items=lines_to_display)
 
     return templates.TemplateResponse("game_of_the_thrones.html",
-                                      {"request": request, "lines": lines_to_display, "pagination": pagination,
-                                       "current_page": page})
+                                      {"request": request, "lines": lines_to_display, "pagination": pagination})
 
 
 @router.get('/select_book')
 async def choosing_a_book(request: Request) -> HTMLResponse:
     return templates.TemplateResponse("books.html", {"request": request})
+
+
+add_pagination(router)
